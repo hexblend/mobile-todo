9716c3a2db3fc3bea051cafb55e8361aea8b5033
Vlad Bibire <vladbibire@Vlads-MacBook-Pro.local>
2019-09-12 22:20:12 +0100

    remove get data from db helper Function

--- src/api/helperFunctions.js
+++ src/api/helperFunctions.js
@@ -2,15 +2,9 @@
 // Helper functions
 //
 import firebase from 'firebase';
-
-// const gotData = (data) => {
-//   const scores = data.val();
-//   const keys = Object.keys(scores);
-//   return keys;
-// };
+const database = firebase.database();
 
 export const addToDatabase = async (link, item) => {
-  const database =  firebase.database();
   const ref = database.ref(link);
   await ref.set(item)
     .then(() => console.log('Item added to the database!'))
